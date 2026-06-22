8660880af454f3445e5c3edac1718b5759a1a6b6
Bc-Xxx <baicaili90@gmail.com>
2026-06-22 17:39:01 +0800

    更新 category.py

--- app/schemas/category.py
+++ app/schemas/category.py
@@ -5,16 +5,22 @@ from pydantic import BaseModel
 
 class CategoryCreate(BaseModel):
     # 前端传什么？
-    name: str         # 分类名，必填
+    name: str  # 分类名，必填
     parent_id: int | None = None  # 父分类 ID，不传就是顶层分类
+
 
 class CategoryOut(BaseModel):
     # API 返回什么？
     id: int
     name: str
     parent_id: int | None = None
-    created_at: datetime            # 从 FatherBase 继承来的
+    created_at: datetime  # 从 FatherBase 继承来的
     updated_at: datetime | None = None
 
     class Config:
-        from_attributes = True      # 告诉 Pydantic 可以从 ORM 对象读取
+        from_attributes = True  # 告诉 Pydantic 可以从 ORM 对象读取
+
+
+class CategoryUpdate(BaseModel):
+    name: str | None = None
+    parent_id: int | None = None
